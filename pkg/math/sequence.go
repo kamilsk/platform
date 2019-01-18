39b333a17263c78bad5830b3f4aaf900ddd8a4df
Kamil Samigullin <kamil@samigullin.info>
2019-01-19 00:27:45 +0300

    add reducer to pkg/math

--- pkg/math/sequence.go
+++ pkg/math/sequence.go
@@ -1,5 +1,86 @@
 package math
 
+import (
+	"math"
+	"sort"
+)
+
+// Reduce wraps sequence to perform some aggregate operations above it.
+func Reduce(sequence ...int) interface {
+	// Average returns the average value of the sequence.
+	Average() float64
+	// Median returns the median value of the sequence.
+	Median() float64
+	// Sum returns the sum of the sequence.
+	Sum() int
+} {
+	return reducer(sequence)
+}
+
+type reducer []int
+
+// Average returns the average value of the sequence.
+func (sequence reducer) Average() float64 {
+	if len(sequence) == 0 {
+		return 0
+	}
+	return float64(sequence.Sum()) / float64(len(sequence))
+}
+
+func (sequence reducer) Count() int {
+	return len(sequence)
+}
+
+func (sequence reducer) Maximum() int {
+	if len(sequence) == 0 {
+		return 0
+	}
+	max := math.MinInt64
+	for _, num := range sequence {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+// Median returns the median value of the sequence.
+func (sequence reducer) Median() float64 {
+	size := len(sequence)
+	if size == 0 {
+		return 0
+	}
+	sorted := make([]int, size)
+	copy(sorted, sequence)
+	sort.Ints(sorted)
+	if size%2 == 0 {
+		return (float64(sorted[size/2-1]) + float64(sorted[size/2])) / 2
+	}
+	return float64(sorted[size/2])
+}
+
+func (sequence reducer) Minimum() int {
+	if len(sequence) == 0 {
+		return 0
+	}
+	min := math.MaxInt64
+	for _, num := range sequence {
+		if num < min {
+			min = num
+		}
+	}
+	return min
+}
+
+// Sum returns the sum of the sequence.
+func (sequence reducer) Sum() int {
+	sum := 0
+	for _, num := range sequence {
+		sum += num
+	}
+	return sum
+}
+
 // Sequence returns an empty slice with the specified size.
 //
 //     for range Sequence(5) {
--- pkg/math/sequence_test.go
+++ pkg/math/sequence_test.go
@@ -9,6 +9,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestReducer_Average(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []int
+		expected float64
+	}{
+		{"nil, zero average", nil, 0},
+		{"empty, zero average", []int{}, 0},
+		{"normal case", []int{1, 2, 3}, 2},
+		{"fractional", []int{1, 2, 3, 4, 5, 6}, 3.5},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, Reduce(tc.sequence...).Average())
+		})
+	}
+}
+
+func TestReducer_Median(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []int
+		expected float64
+	}{
+		{"nil, zero median", nil, 0},
+		{"empty, zero median", nil, 0},
+		{"even, sorted", []int{1, 2, 3, 4}, 2.5},
+		{"even, unsorted", []int{2, 1, 4, 3}, 2.5},
+		{"odd, sorted", []int{1, 2, 3, 4, 5}, 3},
+		{"odd, unsorted", []int{3, 1, 2, 4, 5}, 3},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, Reduce(tc.sequence...).Median())
+		})
+	}
+}
+
+func TestReducer_Sum(t *testing.T) {
+	tests := []struct {
+		name     string
+		sequence []int
+		expected int
+	}{
+		{"nil, zero sum", nil, 0},
+		{"empty, zero sum", []int{}, 0},
+		{"positive sum", []int{1, 2, 3}, 6},
+		{"negative sum", []int{-1, -2, -3}, -6},
+		{"mixed, zero sum", []int{-1, -2, 3}, 0},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, Reduce(tc.sequence...).Sum())
+		})
+	}
+}
+
 func TestSequence(t *testing.T) {
 	tests := []struct {
 		name string
