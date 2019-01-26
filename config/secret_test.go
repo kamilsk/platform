4ed76c00e389474acb776553e6c19f1b8e9ea383
Kamil Samigullin <kamil@samigullin.info>
2019-01-26 18:32:15 +0300

    port config.Secret from github.com/kamilsk/go-kit

---
+++ config/secret_test.go
@@ -0,0 +1,67 @@
+package config_test
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"strings"
+	"testing"
+
+	. "github.com/kamilsk/platform/config"
+	"github.com/stretchr/testify/assert"
+	yaml "gopkg.in/yaml.v2"
+)
+
+func TestSecret_Printing(t *testing.T) {
+	type password struct {
+		XMLName struct{} `json:"-"        xml:"password"   yaml:"-"`
+		Value   Secret   `json:"password" xml:"value,attr" yaml:"password"`
+	}
+	secret := password{Value: "secret"}
+
+	tests := []struct {
+		name    string
+		marshal func(password) ([]byte, error)
+	}{
+		{
+			"print by `%#v`",
+			func(pass password) ([]byte, error) {
+				str := fmt.Sprintf("%#v", pass)
+				return []byte(str), nil
+			},
+		},
+		{
+			"print by `%s`",
+			func(pass password) ([]byte, error) {
+				str := fmt.Sprintf("%s", pass.Value)
+				return []byte(str), nil
+			},
+		},
+		{
+			"json marshal",
+			func(pass password) ([]byte, error) {
+				return json.Marshal(pass)
+			},
+		},
+		{
+			"xml marshal",
+			func(pass password) ([]byte, error) {
+				return xml.Marshal(pass)
+			},
+		},
+		{
+			"yaml marshal",
+			func(pass password) ([]byte, error) {
+				return yaml.Marshal(pass)
+			},
+		},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			raw, err := tc.marshal(secret)
+			assert.NoError(t, err)
+			assert.False(t, strings.Contains(string(raw), string(secret.Value)))
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/stretchr/testify v1.3.0
+	gopkg.in/yaml.v2 v2.2.2
 )
