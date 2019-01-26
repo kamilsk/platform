9c9a885876c4fca03a9d9c3279a44825169bc417
Kamil Samigullin <kamil@samigullin.info>
2019-01-26 15:26:15 +0300

    port pkg/fn from github.com/kamilsk/go-kit

---
+++ pkg/fn/must_test.go
@@ -0,0 +1,42 @@
+package fn_test
+
+import (
+	"errors"
+	"testing"
+
+	. "github.com/kamilsk/platform/pkg/fn"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMust(t *testing.T) {
+	tests := []struct {
+		name     string
+		actions  []func() error
+		assert func(assert.TestingT, assert.PanicTestFunc, ...interface{}) bool
+	}{
+		{
+			"with panic",
+			[]func() error{
+				func() error { return nil },
+				func() error { return errors.New("raise panic") },
+				func() error { return nil },
+			},
+			assert.Panics,
+		},
+		{
+			"without panic",
+			[]func() error{
+				func() error { return nil },
+				func() error { return nil },
+				func() error { return nil },
+			},
+			assert.NotPanics,
+		},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			tc.assert(t, func() { Must(tc.actions...)} )
+		})
+	}
+}
