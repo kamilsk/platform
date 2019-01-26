624fbd88c9d242bcc28d23640834f2bf81711400
Kamil Samigullin <kamil@samigullin.info>
2019-01-26 15:41:08 +0300

    exclude vendor dir

--- pkg/sync/safe_test.go
+++ pkg/sync/safe_test.go
@@ -14,12 +14,21 @@ func TestSafe(t *testing.T) {
 		action func() error
 		closer func(error)
 	}{
-		{"with error", func() error { return errors.New("error") },
-			func(err error) { assert.EqualError(t, err, "error") }},
-		{"with panic", func() error { panic(errors.New("panic")) },
-			func(err error) { assert.EqualError(t, err, "unexpected panic handled: panic") }},
-		{"without anything", func() error { return nil },
-			func(err error) { assert.NoError(t, err) }},
+		{
+			"with error",
+			func() error { return errors.New("error") },
+			func(err error) { assert.EqualError(t, err, "error") },
+		},
+		{
+			"with panic",
+			func() error { panic(errors.New("panic")) },
+			func(err error) { assert.EqualError(t, err, "unexpected panic handled: panic") },
+		},
+		{
+			"without anything",
+			func() error { return nil },
+			func(err error) { assert.NoError(t, err) },
+		},
 	}
 	for _, test := range tests {
 		tc := test
