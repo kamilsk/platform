62adaf056252ee1ca7632324553c9b3dda0605cf
Kamil Samigullin <kamil@samigullin.info>
2019-01-27 11:36:13 +0300

    add Repeat func to pkg/math

---
+++ pkg/fn/repeat_test.go
@@ -0,0 +1,29 @@
+package fn_test
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	. "github.com/kamilsk/platform/pkg/fn"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepeat(t *testing.T) {
+	tests := []struct {
+		name  string
+		times int
+	}{
+		{"zero times", 0},
+		{"constant times", 5},
+		{"random times", rand.New(rand.NewSource(time.Now().UnixNano())).Intn(1000)},
+	}
+	for _, test := range tests {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			var counter int
+			Repeat(func() { counter++ }, tc.times)
+			assert.Equal(t, tc.times, counter)
+		})
+	}
+}
