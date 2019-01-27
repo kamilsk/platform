479693bd6361e945d7a7b138267d82b3c4bf8022
Kamil Samigullin <kamil@samigullin.info>
2019-01-27 10:17:20 +0300

    change uuid receiver name

--- domain/uuid.go
+++ domain/uuid.go
@@ -8,16 +8,16 @@ var uuid = regexp.MustCompile(`(?i:^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB
 type UUID string
 
 // IsEmpty returns true if the UUID has empty value.
-func (s UUID) IsEmpty() bool {
-	return s == ""
+func (value UUID) IsEmpty() bool {
+	return value == ""
 }
 
 // IsValid returns true if the UUID is compatible with RFC 4122.
-func (s UUID) IsValid() bool {
-	return !(s == "") && uuid.MatchString(string(s)) // IsEmpty and String were inlined manually
+func (value UUID) IsValid() bool {
+	return !(value == "") && uuid.MatchString(string(value)) // IsEmpty and String were inlined manually
 }
 
 // String implements built-in fmt.Stringer interface and returns string representation of the UUID.
-func (s UUID) String() string {
-	return string(s)
+func (value UUID) String() string {
+	return string(value)
 }
