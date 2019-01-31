c86682e3d3b72a0fac4c8d42d36f720f38f59ee8
Kamil Samigullin <kamil@samigullin.info>
2019-01-31 18:54:23 +0300

    add rune concat bench

--- pkg/strings/concat_test.go
+++ pkg/strings/concat_test.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"strings"
 	"testing"
+	"unicode/utf8"
 
 	. "github.com/kamilsk/platform/pkg/strings"
 	"github.com/stretchr/testify/assert"
@@ -30,6 +31,7 @@ func TestConcat(t *testing.T) {
 
 func BenchmarkStringConcatenation(b *testing.B) {
 	b.Run("bytes", func(b *testing.B) {
+		var result string
 		concat := func(strings ...string) string {
 			buf := bytes.NewBuffer(nil)
 			for _, str := range strings {
@@ -40,9 +42,33 @@ func BenchmarkStringConcatenation(b *testing.B) {
 		b.ReportAllocs()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			concat("127.0.0.1", ":", "80")
+			result = concat("127.0.0.1", ":", "80")
 		}
+		assert.Equal(b, "127.0.0.1:80", result)
 	})
+	b.Run("runes", func(b *testing.B) {
+		var result string
+		concat := func(strings ...string) string {
+			var ln int
+			for _, str := range strings {
+				ln += utf8.RuneCountInString(str)
+			}
+			pos, rr := 0, make([]rune, ln)
+			for _, str := range strings {
+				ln = utf8.RuneCountInString(str)
+				copy(rr[pos:pos+ln], []rune(str))
+				pos += ln
+			}
+			return string(rr)
+		}
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			result = concat("127.0.0.1", ":", "80")
+		}
+		assert.Equal(b, "127.0.0.1:80", result)
+	})
+
 	b.Run("concat", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
