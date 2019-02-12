1f7f51974d78c8bb6e00eb9dca2d58a868b57a8d
Kamil Samigullin <kamil@samigullin.info>
2019-02-12 11:03:12 +0300

    review protocol interface

--- protocol/interface.go
+++ protocol/interface.go
@@ -5,7 +5,7 @@ import "context"
 // Interface defines the generic behavior of all protocol listeners.
 type Interface interface {
 	// Listen starts listening to its protocol.
+	// It also listens to Context's Done channel to try to do
+	// a graceful shutdown when it closes.
 	Listen(context.Context) error
-	// Shutdown tries to do a graceful shutdown.
-	Shutdown(context.Context) error
 }
