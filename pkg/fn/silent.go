f6fc46c5a38cf8ab04fdc1da82b23080d6bbdc13
Kamil Samigullin <kamil@samigullin.info>
2019-03-01 15:50:35 +0300

    add Stopwatch to pkg/fn

--- pkg/fn/silent.go
+++ pkg/fn/silent.go
@@ -1,8 +1,8 @@
 package fn
 
-// PrintSilent accepts a result of fmt.*Print* function family
+// PrintSilent accepts a result of fmt.Fprint* function family
 // and allow you to ignore it.
 //
-//  PrintSilent(fmt.Println("ignore the result"))
+//  PrintSilent(fmt.Fprintln(writer, "ignore the result"))
 //
 func PrintSilent(int, error) {}
---
+++ pkg/fn/stopwtch_test.go
@@ -0,0 +1,23 @@
+package fn_test
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/kamilsk/platform/pkg/fn"
+	"github.com/stretchr/testify/assert"
+)
+
+var delta = 10 * time.Millisecond
+
+func TestStopwatch(t *testing.T) {
+	var compare time.Duration
+
+	duration := Stopwatch(func() {
+		start := time.Now()
+		time.Sleep(delta)
+		compare = time.Now().Sub(start)
+	})
+
+	assert.True(t, compare < duration)
+}
