7b5dd23806e7716ba6c6834c7fb29b69ca9e0833
Kamil Samigullin <kamil@samigullin.info>
2019-03-13 17:52:20 +0300

    add reverse of pointer functions

---
+++ pkg/pointer/reverse.go
@@ -0,0 +1,137 @@
+package pointer
+
+// ValueOfBool returns value of pointer or its default value if pointer is nil.
+func ValueOfBool(b *bool) bool {
+	if b == nil {
+		return false
+	}
+	return *b
+}
+
+// ValueOfByte returns value of pointer or its default value if pointer is nil.
+func ValueOfByte(b *byte) byte {
+	if b == nil {
+		return 0
+	}
+	return *b
+}
+
+// ValueOfFloat32 returns value of pointer or its default value if pointer is nil.
+func ValueOfFloat32(f *float32) float32 {
+	if f == nil {
+		return 0.0
+	}
+	return *f
+}
+
+// ValueOfFloat64 returns value of pointer or its default value if pointer is nil.
+func ValueOfFloat64(f *float64) float64 {
+	if f == nil {
+		return 0.0
+	}
+	return *f
+}
+
+// ValueOfInt returns value of pointer or its default value if pointer is nil.
+func ValueOfInt(i *int) int {
+	if i == nil {
+		return 0
+	}
+	return *i
+}
+
+// ValueOfInt16 returns value of pointer or its default value if pointer is nil.
+func ValueOfInt16(i *int16) int16 {
+	if i == nil {
+		return 0
+	}
+	return *i
+}
+
+// ValueOfInt32 returns value of pointer or its default value if pointer is nil.
+func ValueOfInt32(i *int32) int32 {
+	if i == nil {
+		return 0
+	}
+	return *i
+}
+
+// ValueOfInt64 returns value of pointer or its default value if pointer is nil.
+func ValueOfInt64(i *int64) int64 {
+	if i == nil {
+		return 0
+	}
+	return *i
+}
+
+// ValueOfInt8 returns value of pointer or its default value if pointer is nil.
+func ValueOfInt8(i *int8) int8 {
+	if i == nil {
+		return 0
+	}
+	return *i
+}
+
+// ValueOfRune returns value of pointer or its default value if pointer is nil.
+func ValueOfRune(r *rune) rune {
+	if r == nil {
+		return 0x00
+	}
+	return *r
+}
+
+// ValueOfString returns value of pointer or its default value if pointer is nil.
+func ValueOfString(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
+
+// ValueOfUint returns value of pointer or its default value if pointer is nil.
+func ValueOfUint(u *uint) uint {
+	if u == nil {
+		return 0
+	}
+	return *u
+}
+
+// ValueOfUint16 returns value of pointer or its default value if pointer is nil.
+func ValueOfUint16(u *uint16) uint16 {
+	if u == nil {
+		return 0
+	}
+	return *u
+}
+
+// ValueOfUint32 returns value of pointer or its default value if pointer is nil.
+func ValueOfUint32(u *uint32) uint32 {
+	if u == nil {
+		return 0
+	}
+	return *u
+}
+
+// ValueOfUint64 returns value of pointer or its default value if pointer is nil.
+func ValueOfUint64(u *uint64) uint64 {
+	if u == nil {
+		return 0
+	}
+	return *u
+}
+
+// ValueOfUint8 returns value of pointer or its default value if pointer is nil.
+func ValueOfUint8(u *uint8) uint8 {
+	if u == nil {
+		return 0
+	}
+	return *u
+}
+
+// ValueOfUintptr returns value of pointer or its default value if pointer is nil.
+func ValueOfUintptr(u *uintptr) uintptr {
+	if u == nil {
+		return 0x0
+	}
+	return *u
+}
---
+++ pkg/pointer/reverse_test.go
@@ -0,0 +1,229 @@
+package pointer_test
+
+import (
+	"testing"
+
+	. "github.com/kamilsk/platform/pkg/pointer"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValueOfBool(t *testing.T) {
+	tests := []struct {
+		b *bool
+		v bool
+	}{
+		{},
+		{ToBool(true), true},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfBool(test.b))
+	}
+}
+
+func TestValueOfByte(t *testing.T) {
+	tests := []struct {
+		b *byte
+		v byte
+	}{
+		{},
+		{ToByte(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfByte(test.b))
+	}
+}
+
+func TestValueOfFloat32(t *testing.T) {
+	tests := []struct {
+		f *float32
+		v float32
+	}{
+		{},
+		{ToFloat32(7.7), 7.7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfFloat32(test.f))
+	}
+}
+
+func TestValueOfFloat64(t *testing.T) {
+	tests := []struct {
+		f *float64
+		v float64
+	}{
+		{},
+		{ToFloat64(7.7), 7.7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfFloat64(test.f))
+	}
+}
+
+func TestValueOfInt(t *testing.T) {
+	tests := []struct {
+		i *int
+		v int
+	}{
+		{},
+		{ToInt(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfInt(test.i))
+	}
+}
+
+func TestValueOfInt16(t *testing.T) {
+	tests := []struct {
+		i *int16
+		v int16
+	}{
+		{},
+		{ToInt16(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfInt16(test.i))
+	}
+}
+
+func TestValueOfInt32(t *testing.T) {
+	tests := []struct {
+		i *int32
+		v int32
+	}{
+		{},
+		{ToInt32(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfInt32(test.i))
+	}
+}
+
+func TestValueOfInt64(t *testing.T) {
+	tests := []struct {
+		i *int64
+		v int64
+	}{
+		{},
+		{ToInt64(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfInt64(test.i))
+	}
+}
+
+func TestValueOfInt8(t *testing.T) {
+	tests := []struct {
+		i *int8
+		v int8
+	}{
+		{},
+		{ToInt8(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfInt8(test.i))
+	}
+}
+
+func TestValueOfRune(t *testing.T) {
+	tests := []struct {
+		r *rune
+		v rune
+	}{
+		{},
+		{ToRune('a'), 'a'},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfRune(test.r))
+	}
+}
+
+func TestValueOfString(t *testing.T) {
+	tests := []struct {
+		s *string
+		v string
+	}{
+		{},
+		{ToString("a"), "a"},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfString(test.s))
+	}
+}
+
+func TestValueOfUint(t *testing.T) {
+	tests := []struct {
+		u *uint
+		v uint
+	}{
+		{},
+		{ToUint(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfUint(test.u))
+	}
+}
+
+func TestValueOfUint16(t *testing.T) {
+	tests := []struct {
+		u *uint16
+		v uint16
+	}{
+		{},
+		{ToUint16(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfUint16(test.u))
+	}
+}
+
+func TestValueOfUint32(t *testing.T) {
+	tests := []struct {
+		u *uint32
+		v uint32
+	}{
+		{},
+		{ToUint32(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfUint32(test.u))
+	}
+}
+
+func TestValueOfUint64(t *testing.T) {
+	tests := []struct {
+		u *uint64
+		v uint64
+	}{
+		{},
+		{ToUint64(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfUint64(test.u))
+	}
+}
+
+func TestValueOfUint8(t *testing.T) {
+	tests := []struct {
+		u *uint8
+		v uint8
+	}{
+		{},
+		{ToUint8(7), 7},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfUint8(test.u))
+	}
+}
+
+func TestValueOfUintptr(t *testing.T) {
+	tests := []struct {
+		u *uintptr
+		v uintptr
+	}{
+		{},
+		{ToUintptr(0x1), 0x1},
+	}
+	for _, test := range tests {
+		assert.Equal(t, test.v, ValueOfUintptr(test.u))
+	}
+}
