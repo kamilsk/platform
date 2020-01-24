9575454e653f4f6f68e4c49f69dd145515a18077
Kamil Samigullin <kamil@samigullin.info>
2020-01-24 08:50:20 +0300

    deprecate protocol/http.Header

--- protocol/http/header.go
+++ protocol/http/header.go
@@ -14,14 +14,17 @@ const (
 )
 
 // Header extends built-in http.Header.
+// Deprecated: use go.octolab.org/toolkit/protocol/http.Header instead.
 type Header http.Header
 
 // NoCache returns true if the header has no-cache value of cache control.
+// Deprecated: use go.octolab.org/toolkit/protocol/http.Header instead.
 func (header Header) NoCache() bool {
 	return strings.EqualFold(http.Header(header).Get(CacheControlHeader), "no-cache")
 }
 
 // Strict returns true if the header has this value.
+// Deprecated: use go.octolab.org/toolkit/protocol/http.Header instead.
 func (header Header) Strict() bool {
 	var strict bool
 	if v := http.Header(header).Get(XStrictHeader); v != "" {
@@ -30,7 +33,9 @@ func (header Header) Strict() bool {
 	return strict
 }
 
-// Timeout returns the percentage of a timeout value from the header or the fallback value.
+// Timeout returns the percentage of a timeout value from the header
+// or the fallback value.
+// Deprecated: use go.octolab.org/toolkit/protocol/http.Header instead.
 func (header Header) Timeout(fallback time.Duration, percentage float64) time.Duration {
 	d, err := time.ParseDuration(http.Header(header).Get(XTimeoutHeader))
 	if err != nil {
